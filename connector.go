0150eca62d11cca6912c561184ad9ff086b6c949
Di Wu <di.wu@snowflake.com>
2020-12-04 22:56:05 +0000

    make Driver private

--- connector.go
+++ connector.go
@@ -7,24 +7,24 @@ import (
 	"database/sql/driver"
 )
 
-// Driver is the interface for a Snowflake driver
-type Driver interface {
+// snowflakeDriver is the interface for a Snowflake driver
+type snowflakeDriver interface {
 	Open(dsn string) (driver.Conn, error)
 	OpenWithConfig(ctx context.Context, config Config) (driver.Conn, error)
 }
 
-// Connector creates Driver with the specified Config
+// Connector creates connections using the Snowflake driver and config.
 type Connector struct {
-	driver Driver
+	driver snowflakeDriver
 	cfg    Config
 }
 
-// NewConnector creates a new connector with the given SnowflakeDriver and Config.
-func NewConnector(driver Driver, config Config) Connector {
+// NewConnector creates a new connector with the given driver and config.
+func NewConnector(driver snowflakeDriver, config Config) Connector {
 	return Connector{driver, config}
 }
 
-// Connect creates a new connection.
+// Connect creates a new connection using the underlying driver.
 func (t Connector) Connect(ctx context.Context) (driver.Conn, error) {
 	cfg := t.cfg
 	err := fillMissingConfigParameters(&cfg)
@@ -34,7 +34,7 @@ func (t Connector) Connect(ctx context.Context) (driver.Conn, error) {
 	return t.driver.OpenWithConfig(ctx, cfg)
 }
 
-// Driver creates a new driver.
+// Driver returns the underlying driver.
 func (t Connector) Driver() driver.Driver {
 	return t.driver
 }
